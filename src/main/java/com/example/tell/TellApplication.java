0fb7f21f89f2c54a5864ab93d729f469f8036485
jqlliu <2260216963liu@gmail.com>
2023-10-29 20:53:47 -0400

    Added files

---
+++ src/main/java/com/example/tell/TellApplication.java
@@ -0,0 +1,13 @@
+package com.example.tell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tell/TellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
